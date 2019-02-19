17e9578afe86dfe1362e351525c22034519675c3
Abhijeet Singh Chauhan <chauhanabhi1245@gmail.com>
2019-02-19 14:40:06 +0530

    bottom bar ui fixes and back press added on event detailpage

--- app/src/main/java/com/jss/abhi/zealicon/activities/EventDetailActivity.java
+++ app/src/main/java/com/jss/abhi/zealicon/activities/EventDetailActivity.java
@@ -11,10 +11,11 @@ import android.support.annotation.RequiresApi;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-
 import com.jss.abhi.zealicon.R;
 import com.jss.abhi.zealicon.service.NotificationService;
 
@@ -24,6 +25,8 @@ import java.util.Date;
 import java.util.Locale;
 
 public class EventDetailActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -39,18 +42,29 @@ public class EventDetailActivity extends AppCompatActivity {
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
 
         // finally change the color
-        window.setStatusBarColor(ContextCompat.getColor(this,R.color.colorPrimary));
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
 
-        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.toolbar_layout);
         collapsingToolbarLayout.setTitle("Code in Pair");
         collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.white));
-
-       // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-       // getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // getSupportActionBar().setDisplayShowHomeEnabled(true);
         //toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.app_white));
 
 
-        notifyme();
+        /**
+         * this function will be called when star or bell is pressed
+         */
+
+        //notifyme();
 
 
     }
@@ -77,9 +91,9 @@ public class EventDetailActivity extends AppCompatActivity {
         PendingIntent pi = PendingIntent.getBroadcast(getApplicationContext(), notificationid, intent, PendingIntent.FLAG_ONE_SHOT);
         sf.edit().putInt("key", notificationid + 1).apply();
         AlarmManager am = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
-        am.set(AlarmManager.RTC_WAKEUP, millis-3600000, pi);
+        am.set(AlarmManager.RTC_WAKEUP, millis - 3600000, pi);
         AlarmManager am2 = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
-        am2.set(AlarmManager.RTC_WAKEUP, millis-300000, pi);
+        am2.set(AlarmManager.RTC_WAKEUP, millis - 300000, pi);
     }
 
 }
